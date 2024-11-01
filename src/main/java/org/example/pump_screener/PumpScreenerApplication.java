ca774b14328146b1421a2babc6842711bd5f6b5e
User <artem.zakrzhevski@mail.ru>
2024-11-01 21:16:24 +0300

    Initial commit

---
+++ src/main/java/org/example/pump_screener/PumpScreenerApplication.java
@@ -0,0 +1,13 @@
+package org.example.pump_screener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpScreenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PumpScreenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pump_screener/PumpScreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pump_screener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpScreenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
